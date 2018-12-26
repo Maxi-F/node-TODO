d75416eb16e98ca2203fe608cd8873cb71f66c5f
Maxi <maxifeldman@hotmail.com>
2018-12-26 17:31:07 -0300

    fixed repeated code logic

--- por-hacer/por-hacer.js
+++ por-hacer/por-hacer.js
@@ -41,29 +41,35 @@ const crear = (descripcion) => {
     return porHacer;
 }
 
-const actualizar = (descripcion, completado = true) => {
-    cargarDB();
-    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
-    if (index >= 0) {
-        listadoPorHacer[index].completado = completado;
-        guardarDB();
-        return true;
-    } else {
-        return false;
-    }
+const indexDeLaTarea = (descripcion) => {
+    let indexTarea = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    return indexTarea;
 }
 
-const borrar = (descripcion) => {
-        cargarDB();
-        let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
-        if (index >= 0) {
-            listadoPorHacer.splice(index, 1);
-            guardarDB();
-            return true;
-        } else {
-            return false;
-        }
-    } // mucha repeticion de logica, ver como no repetir!!!!
+const ejecutarCambio = (indexTarea, funcion, ...args) => {
+    funcion(indexTarea, args);
+}
+
+const actualizarCompletado = (indexTarea, [completado]) => {
+    listadoPorHacer[indexTarea].completado = completado;
+}
+
+const borrarTarea = (indexTarea, []) => {
+    listadoPorHacer.splice(indexTarea, 1);
+}
+
+const realizarCambios = (descripcion, funcion, ...args) => {
+    cargarDB();
+    let indexTarea = indexDeLaTarea(descripcion);
+    if (indexTarea < 0) return false;
+    ejecutarCambio(indexTarea, funcion, args);
+    guardarDB();
+    return true;
+}
+
+const actualizar = (descripcion, completado = true) => realizarCambios(descripcion, actualizarCompletado, completado)
+
+const borrar = (descripcion) => realizarCambios(descripcion, borrarTarea)
 
 module.exports = {
     crear,
